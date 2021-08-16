9b1a798aca03f515582714623f94b7bb4e207635
abdumamdouh <eabdogfx@gmail.com>
2021-08-16 19:26:22 +0200

    middleware added

---
+++ src/middleware/logger.js
@@ -0,0 +1,13 @@
+// logger middleware for debugging
+
+const logger = (store) => (next) => (action) => {
+  console.group(action.type);
+  console.log("the action: ", action);
+  const returnValue = next(action);
+  console.log("the new state: ", store.getState());
+  console.groupEnd();
+
+  return returnValue;
+};
+
+export default logger;
