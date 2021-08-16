1c4a5dbad2f6fd2a7ff7cbf14e0957b8591e37e2
abdumamdouh <eabdogfx@gmail.com>
2021-08-16 18:52:45 +0200

    implement store

--- src/index.js
+++ src/index.js
@@ -1,6 +1,18 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import './index.css'
-import App from './components/App'
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./components/App";
 
-ReactDOM.render(<App />, document.getElementById('root'))
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+
+import reducers from "./reducers";
+
+const store = createStore(reducers);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
